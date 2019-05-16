d31f1ef2146c8dbdaa32aa6a190451ceca6ef24b
Aviciena Santoso <22242172@student.uwa.edu.au>
2019-05-16 21:27:57 +0800

    added homepage, first template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 import datetime
 
 @app.route('/')
 @app.route('/home')
-def index():
-    return f"Hello world! --- Initial commit for UWA CITS3403 Assignment. --- Name: Aviciena Santoso --- The time is {datetime.datetime.now()}"
+def home():
+    user = {'username': 'Miguel'}
+    return render_template('home.html', user=user)
