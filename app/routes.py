7778ff28a30769c7daaefd0d0c16a27b73700c5d
Aviciena Santoso <22242172@student.uwa.edu.au>
2019-05-17 10:33:06 +0800

    added overview

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,24 @@ import datetime
 @app.route('/')
 @app.route('/feed')
 def feed():
+    return render_template('feed.html', title="Feed - QRVA")
+
+@app.route('/overview')
+def overview():
     user = {'username': 'Aviciena Santoso'}
-    return render_template('feed.html', title="Feed - QRVA", user=user)
+    return render_template('overview.html', title="Overview - QRVA")
+
+@app.route('/profile')
+def profile():
+    user = {'username': 'Aviciena Santoso'}
+    return render_template('profile.html', title="Profile - QRVA")
+
+@app.route('/users')
+def users():
+    user = {'username': 'Aviciena Santoso'}
+    return render_template('users.html', title="Edit Users - QRVA")
+
+@app.route('/vote')
+def vote():
+    user = {'username': 'Aviciena Santoso'}
+    return render_template('users.html', title="Edit Users - QRVA")
