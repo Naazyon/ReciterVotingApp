08dfa07d21e4abfa23834dba39d9d5aa6d98b714
Aviciena Santoso <22242172@student.uwa.edu.au>
2019-05-19 13:39:45 +0800

    trivial commit

--- app/models.py
+++ app/models.py
@@ -3,8 +3,12 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     permissions = db.Column()
+    name = db.Column()
     email = db.Column()
     password_hash = db.Column()
     online = db.Column()
     numVotes = db.Column()
     numReviews = db.Column()
+    
+    def __repr__(self):
+        return f'<User {self.name}>'
